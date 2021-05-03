974a1ce8e9ec88ada54b06a7ba1d4eb0a3c9078d
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-03 17:55:30 -0300

    add arquivos.py

---
+++ sintax.py
@@ -0,0 +1,14 @@
+numeros = [1, 2, 3, 4, 5]
+novos_numeros = [numero for numero in numeros]
+
+'''
+Similar usando laço for
+
+novos_numeros = []
+for numero in numeros:
+    novos_numeros.append(numero)
+'''
+
+
+ numeros[0] = 20
+ print(novos_numeros)
